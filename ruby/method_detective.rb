08e45d78482400629553a7f2a8af3da051e5cfbe
Paul Duca <paul.r.duca@gmail.com>
2016-08-06 22:30:01 -0400

    Completed method detective work

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,58 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
+"iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
 
-# "zom".<???>
+
+"zom".sub("o","oo")
+"zom".gsub("o","oo")
+"zom".insert(1,"o")
+"zom".replace("zoom")
 # => “zoom”
 
-# "enhance".<???>
+
+"enhance".center("enhance".length + 8)
+"enhance".prepend("    ").concat("    ")
+# Replace works for pretty much every instance so I stopped putting it in
+"zom".replace("    enhance    ")
 # => "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
+
+"Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
+
+"the usual".concat(" suspects")
+"the usual".insert(9," suspects")
 #=> "the usual suspects"
 
-# " suspects".<???>
+
+
+" suspects".insert(0, "the usual")
+" suspects".prepend("the usual")
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
+
+"The case of the disappearing last letter".chomp('r')
+"The case of the disappearing last letter".chop
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
+
+"The mystery of the missing first letter".slice(1,38)
+"The mystery of the missing first letter".reverse.chop.reverse
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+
+"Elementary,    my   dear        Watson!".split.join(" ")
+"Elementary,    my   dear        Watson!".squeeze(" ")
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
+
+"z".ord
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
 
-# "How many times does the letter 'a' appear in this string?".<???>
+
+"How many times does the letter 'a' appear in this string?".count('a')
 # => 4
