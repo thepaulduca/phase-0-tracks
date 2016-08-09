b0414617ed5befaa258a5eb6e5d81baafd831755
Paul Duca <paul.r.duca@gmail.com>
2016-08-08 23:00:14 -0400

    Call the data

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -21,7 +21,7 @@ city[:nyc] << nyc_streets
 nyc_sights = ['statue of liberty', 'empire state building', 'yankee stadium','citi field', 'MOMA']
 city[:nyc] << nyc_sights
 
-nyc_famous = ['Notorious BIG', 'Bernie Sanders', 'Jay -z', 'Donald trump', 'Michael Jordan', 'J-lo']
+nyc_famous = ['Notorious BIG', 'Bernie Sanders', 'Jay -z', ['donald trump', 'donald drumpf'] , 'Michael Jordan', 'J-lo']
 city[:nyc] <<  nyc_famous
 
 #Philly arrays
@@ -29,7 +29,7 @@ city[:nyc] <<  nyc_famous
 philly_streets = ['broad st', 'norris','22nd st', "fairmount", '13th st']
 city[:philly] << philly_streets
 
-philly_sights = ['liberty bell', 'the linc', 'the philly art museum', 'ritten house square']
+philly_sights = ['liberty bell', 'the linc', 'the philly art museum', 'ritten house square', 'The Temple University']
 city[:philly] << philly_sights
 
 philly_famous = ['billy cosby', 'will smith', 'warren buffet', 'wilt the stilt', 'brad cooper']
@@ -43,7 +43,7 @@ city[:chicago] << chicago_streets
 chicago_sights = ['art institute','shedd aquarium', 'millennium park', 'navy pier']
 city[:chicago] << chicago_sights
 
-chicago_famous = ['kanye west', 'd=rose', 'POTUS','harrison ford', 'Hugh Heff']
+chicago_famous = ['kanye west', 'd-rose', 'Barack Obama','harrison ford', 'Hugh Heff']
 city[:chicago] << chicago_famous
 
 
@@ -55,7 +55,30 @@ city[:dallas] << dallas_streets
 dallas_sights = ['cowboy stadium','dealey plaza', 'world aquarium','klyde warren park']
 city[:dallas] << dallas_sights
 
-dallas_famous = ['usher', 'erkah badu', 'JFK', 'Jayne Mansfield']
+dallas_famous = ['usher', 'erkah badu', 'JFK', 'jayne mansfield']
 city[:dallas] << dallas_famous
 
-puts city
+
+# Access the data, THE FUN PART ... i guess? 
+
+puts "I went to #{city[:philly][1][4]}"
+
+puts "My favorite place is the one and only #{city[:nyc][1][2]}"
+
+puts "The president of the United states is #{city[:chicago][2][2]}"
+
+puts "#{city[:dallas][2][1]} makes amazing music"
+
+puts city[:nyc]
+
+puts city[:nyc][1]
+
+puts city[:chicago].assoc('kanye west')
+
+
+
+
+
+
+
+
