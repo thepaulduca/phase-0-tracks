21339a0bb19c28e94b4eb570a52e3b64d7a35d7f
Paul Duca <paul.r.duca@gmail.com>
2016-08-16 00:50:24 -0400

    clunky but working team system

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -56,7 +56,7 @@ puts "Welcome to the Basketball Player Creator!"
 puts "How many players would like to create?"
 num_of_players = gets.chomp
 num_of_players.to_i.times do |x|
-	puts "what position would you like player one to be?"
+	puts "what position would you like  one to be?"
 	position = gets.chomp.downcase
 	puts "how tall are they? (type feet, then space and then inches)"
 	player_height = gets.chomp.split(' ')
@@ -65,7 +65,102 @@ end
 
 p '----------------'
 
+####new thing !
+
+
+bulls = []
+lakers = []
+
+
+players.map do |player|
+	puts "Would you like him to play for the bulls or the lakers?"
+	puts "You are choosing for : #{player.position}"
+	team_loop = true
+	until !team_loop
+		puts "type the team name you would each player to join!!"
+		team_name = gets.chomp
+		if team_name.downcase == 'bulls'
+			puts "the player joined the Chicago Bulls"
+			player.location = team_name
+			bulls << player 
+			team_loop = false
+		elsif team_name.downcase == 'lakers'
+			puts "the player joined the LA lakers"
+			player.location = team_name
+			lakers << player
+			team_loop = false
+		else
+			puts "You cant type! try again"
+		end
+	end
+end
+
+	p '----------------'
+	puts "bulls players:"
+	puts bulls.length
+	bulls.each do |player|
+		puts player.position
+	end
+	p '----------------'
+	puts "lakers players"
+	puts lakers.length
+	lakers.each do |player|
+		puts player.position
+	end
+	p '----------------'
+
+
+ 
+unless bulls.length == lakers.length
+
+	puts "The teams are uneven"
+	puts "would you like to create another player to make it even?"
+	puts "type even to even up"
+	even_teams = gets.chomp
+	if even_teams == 'even'
+		puts "what position would you like player one to be?"
+		position = gets.chomp.downcase
+		puts "how tall are they? (type feet, then space and then inches)"
+		player_height = gets.chomp.split(' ')
+		new_player = Basketball_player.new(position, player_height)
+		players << new_player
+		if bulls.length > lakers.length
+			lakers << new_player
+		else
+			bulls << new_player
+		end
+	else
+		puts "I like it."
+	end
+
+
+	p '----------------'
+	puts "bulls players:"
+	puts bulls.length
+	bulls.each do |player|
+		puts player.position
+	end
+	p '----------------'
+	puts "lakers players"
+	puts lakers.length
+	lakers.each do |player|
+		puts player.position
+	end
+	p '----------------'
+
+end 
+
+
+
+p '----------------'
+puts "All players players"
+puts players.length 
 puts players
+
+### if broken delete that first ^^
+
+
+
 
 p '----------------'
 
@@ -84,6 +179,7 @@ puts "Type the position of the position you want to shoot (from 2)!"
 shooting_p = gets.chomp.downcase
 players.each do |player|
 		if player.position == shooting_p
+			puts player.location
 			player.shoot
 		end
 	end
@@ -100,6 +196,7 @@ puts '-----------------'
 puts "Type the position of the player you want to shoot from 3!"
 shooting_p = gets.chomp.downcase
 players.each do |player|
+	puts player.location
 	if player.position == shooting_p
 		player.shoot_3
 	end
@@ -107,14 +204,20 @@ end
 
 puts '-----------------'
 
-puts "Here is how your players played!"
+puts "Here is how the teams  played!"
 puts '-----------------'
-players.each do |player|
+lakers.each do |player|
 	puts  player.position
 	puts "Points: "
 	puts "#{player.points_scored}"
-	p "---------------"
 end
+	p "---------------"
+bulls.each do |player|
+	puts  player.position
+	puts "Points: "
+	puts "#{player.points_scored}"
+end
+
 
 
 
@@ -138,3 +241,4 @@ end
 kobe.points_scored
 
 =end
+
